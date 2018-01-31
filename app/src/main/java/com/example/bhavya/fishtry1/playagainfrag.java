23ed7fca891e824ecedf52140daf1a70cbde5b96
Bhavya Gupta <bhavya.gupta97@gmail.com>
2018-01-31 22:46:03 +0530

    Update playagainfrag.java

---
+++ app/src/main/java/com/example/bhavya/fishtry1/playagainfrag.java
@@ -0,0 +1,47 @@
+package com.example.bhavya.fishtry1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class playagainfrag extends Fragment {
+
+
+    public  MainActivity p;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                            final Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_playagainfrag, container, false);
+
+        Button playAgain = (Button) view.findViewById(R.id.button2);
+        Button exit = (Button)view.findViewById(R.id.button3);
+        playAgain.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+
+                        p.count=0;
+                    }
+                }
+        );
+        return view;
+    }
+}
