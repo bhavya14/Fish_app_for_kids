a85745c77481df366e58d848036feccbe4c590cc
Bhavya Gupta <bhavya.gupta97@gmail.com>
2018-01-31 23:10:21 +0530

    Update Winningmessage.java

--- app/src/main/java/com/example/bhavya/fishtry1/Winningmessage.java
+++ app/src/main/java/com/example/bhavya/fishtry1/Winningmessage.java
@@ -39,3 +39,57 @@ public class Winningmessage extends Fragment  {
         return v;
     }
 }
+
+
+
+
+<<<<<<< HEAD
+package com.example.bhavya.fishtry1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Winningmessage extends Fragment  {
+
+
+    Button b1,b2;
+    MainActivity mainActivity;
+    @Nullable
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.winning,container,false);
+        b1= (Button) v.findViewById(R.id.PlayAgain);
+        b2= (Button) v.findViewById(R.id.exit);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                Log.d("hey","-----------------------");
+                mainActivity.play();
+                 mainActivity.onCreate(null);
+            }
+        });
+
+
+        
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.finish();
+            }
+        });
+
+        return v;
+    }
+}
