422661eb86f8af87924dfdee65d92e02794960be
Bhavya Gupta <bhavya.gupta97@gmail.co>
2018-01-31 22:35:19 +0530

    adding frag

---
+++ app/src/main/java/com/example/bhavya/fishtry1/fragment1.java
@@ -0,0 +1,51 @@
+package com.example.bhavya.fishtry1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import pl.droidsonroids.gif.GifTextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class fragment1 extends Fragment {
+
+    GifTextView fireworks ;
+       MainActivity act;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //final RelativeLayout buttonContainer = (RelativeLayout) getView().findViewById(R.id.a12);
+       View view =  inflater.inflate(R.layout.fragment_fragment1, container,false);
+        fireworks = (GifTextView) view.findViewById(R.id.fireworks1);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 2000ms
+                fireworks.animate().alpha(0f);
+                calling();
+
+
+            }
+        }, 2000);
+
+
+        // Inflate the layout for this fragment
+
+        return view;
+    }
+
+    public void calling()
+    {
+        act.makingvisible();
+
+    }
+}
